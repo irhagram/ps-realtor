1b6b56be8ecd294dd876788a075298c26ef95283
irhagram <70169238+irhagram@users.noreply.github.com>
2023-08-12 09:17:34 +0700

    adjust shared

--- shared/config.lua
+++ shared/config.lua
@@ -1,13 +1,13 @@
 Config = Config or {}
 
 -- Set your Real Estate here
-Config.RealtorJobName = "realestate"
+Config.RealtorJobName = "realtor"
 
 -- Set this value to true if you want to use the command to open the Housing Menu(Realtor Menu)
 Config.UseCommand = true
 
 -- Set this value to TRUE if you want to use the Config.ItemName as a usable Item to open the Housing Menu(Realtor Menu)
-Config.UseItem = true
+Config.UseItem = false
 
 --Set this value to FALSE if u DON'T want to play the tablet animation when using /housing or the usable item
 Config.PlayAnimation = true
@@ -22,10 +22,10 @@ Config.ItemName = "tablet"  -- Item name to make it usable.
 --Commisions is handled in ps-housing config.
 
 Config.RealtorPerms = {
-    manageProperty = 1, --minimum grade to manage property | default 1
+    manageProperty = 0, --minimum grade to manage property | default 1
     changePropertyForSale = 0, --minimum grade to list property | default 0
     sellProperty = 0, --minimum grade to sell property | default 0
-    listNewProperty = 2, --minimum grade to list property | default 2
-    deleteProperty = 2, --minimum grade to delete property | default 2
-    setApartments = 2, --minimum grade to set apartments for players | default 2
+    listNewProperty = 0, --minimum grade to list property | default 2
+    deleteProperty = 0, --minimum grade to delete property | default 2
+    setApartments = 0, --minimum grade to set apartments for players | default 2
 }
